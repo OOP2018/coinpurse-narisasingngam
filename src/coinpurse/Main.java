118bd283edc1b73ebb3ecab807fd2b202d38779b
narisasingngam <mmintttt@gmail.com>
2018-02-18 11:16:18 +0700

    finish

--- src/coinpurse/Main.java
+++ src/coinpurse/Main.java
@@ -1,11 +1,33 @@
 package coinpurse;
- 
+
+import java.util.ResourceBundle;
+
 /**
  * A main class to create objects and connect objects together.
  * The user interface needs a reference to coin purse.
  * @author your name
  */
 public class Main {
+	public static MoneyFactory init() {
+		ResourceBundle bundle = ResourceBundle.getBundle("purse");
+
+		String factoryclass = bundle.getString("moneyfactory");
+		MoneyFactory factory = null;
+
+		try {
+			factory = (MoneyFactory) Class.forName(factoryclass).newInstance();
+		} catch (ClassCastException cce) {
+			// the object could not be cast to type MoneyFactory
+			System.out.println(factory + " is not type MoneyFactory");
+		} catch (Exception ex) {
+			// any other exception means we could not create an object
+			System.out.println("Error creating MoneyFactory " + ex.getMessage());
+		}
+		// if no factory then quit
+		if (factory == null)
+			System.exit(1);
+		return factory;
+	}
 
     /**
      * Configure and start the application.
